de303b5a75444800a01f9f9a92728f34b87afa4e
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2020-07-16 23:22:47 +0200

    Updates

--- precache-manifest.b447638cc123a5bede2d25354e5a4e93.js
+++ precache-manifest.b447638cc123a5bede2d25354e5a4e93.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "085a07c9b287b5df62bad8d1c60caa7a",
+    "revision": "eb78dc9f6e355e2ad6086c58fd4bf28b",
     "url": "/BookingApp/index.html"
   },
   {
-    "revision": "59951eaa5f3fdf0469c1",
+    "revision": "bcf1ffbb385217791b4a",
     "url": "/BookingApp/static/css/main.b3c7a8b6.chunk.css"
   },
   {
-    "revision": "5a26c620dfa5e92c81c4",
-    "url": "/BookingApp/static/js/2.d196e02a.chunk.js"
+    "revision": "5d2936d23a4c61123d09",
+    "url": "/BookingApp/static/js/2.c3708eca.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/BookingApp/static/js/2.d196e02a.chunk.js.LICENSE.txt"
+    "url": "/BookingApp/static/js/2.c3708eca.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "59951eaa5f3fdf0469c1",
-    "url": "/BookingApp/static/js/main.6d9e58e4.chunk.js"
+    "revision": "bcf1ffbb385217791b4a",
+    "url": "/BookingApp/static/js/main.99ace5d4.chunk.js"
   },
   {
     "revision": "2b1f63b4844dbfef15a8",
@@ -26,6 +26,18 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "c7fedfa061fe1cdacbbfc58ca273898f",
     "url": "/BookingApp/static/media/chevron-thin-right.c7fedfa0.svg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/BookingApp/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/BookingApp/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/BookingApp/static/media/hotel-3.7af5efbc.jpg"
   },
   {
     "revision": "3203e8f6c24947aa32f9deaaa7b83b67",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BookingApp/precache-manifest.7b9371b9ea7bf157b654eabcae2fef88.js"
+  "/BookingApp/precache-manifest.b447638cc123a5bede2d25354e5a4e93.js"
 );
 
 self.addEventListener('message', (event) => {
